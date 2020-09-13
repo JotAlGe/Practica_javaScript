8010a9d23390f36ba518c1a2d10e0d6d9df3a62a
Juanhchis <juanchismo10@gmail.com>
2020-09-13 15:24:20 -0300

    Se agregaron nuevos estilos, como imagen de fondo y color de la tipografía

--- agregar_elementos.js
+++ agregar_elementos.js
@@ -1,10 +1,9 @@
-
      const btnSave = document.getElementById('btn-save');
      
+     //crea elementos 
      const addElement = () => {
-          const divContainer = document.getElementById('container');
           const name = document.getElementById('nombre').value;
-          //const p_mostrar = document.getElementById('p_mostrar');
+
           //crea el elemento
           const newElement = document.createElement('p');
           const newContent = document.createTextNode(name);
